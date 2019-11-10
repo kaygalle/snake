a3e4847208862d2ca7ac72da44ef99a769ed30f7
OlegArtamonov <ArtamonovAV2003@gmail.com>
2019-11-10 14:58:56 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -3,32 +3,48 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Timers;
+using System.Diagnostics;
 namespace Snake{
   class Programm{
     public static void Main(){
-      int m=5;
+      int m=10;
       int x=0;
       int y=0;
-      int t;// 1-Up 2-Down 3-Left 4-Right
-      ConsoleKeyInfo direct;
+      int t=0;// 1-Up 2-Down 3-Left 4-Right
       char [,] map = new char [m,m];
-      printmap(m,map,x,y);
+      while(true){
+        fillmap(m,map,ref x,ref y);
+        move(ref x,ref y,ref t,m);
+        Thread.Sleep(200);
+        Console.Clear();
+        printmap(m,map);
+        if(t==5){
+          break;
+        }
+        }
     }
-    public void printmap(int m,char [,] map,int x,int y){
+    public static void fillmap(int m,char [,] map,ref int x,ref int y){
       for(int i=0;i<m;i++){
         for(int z=0;z<m;z++){
-          if(i==x&&z==y){
-            Console.Write('1');
+          if(x==z&&i==y){
+            map[i,z]='1';
           }else{
+            map[i,z]='0';
+          }
+        }
+        }
+      }
+    public static void printmap(int m,char [,] map){
+      for(int i=0;i<m;i++){
+        for(int z=0;z<m;z++){
           Console.Write(map[i,z]);
         }
-        }
         Console.WriteLine(" ");
+        }
       }
-    }
-    public void move(int x,int y,int t,ConsoleKeyInfo direct,int m){
+    public static void move(ref int x,ref int y,ref int t,int m){
       if(Console.KeyAvailable){
-        direct=Console.ReadKey(true);
+        ConsoleKeyInfo direct=Console.ReadKey(true);
         if(direct.Key==ConsoleKey.UpArrow){
           if(y>0){
             y--;
@@ -53,7 +69,27 @@ namespace Snake{
             t=4;
           }
         }
+        if(direct.Key==ConsoleKey.Escape){
+          t=5;
+        }
+      }else{
+        if(t==1&&y>0){
+          y--;
+        }else
+        if(t==2&&y<m-1){
+          y++;
+        }else
+        if(t==3&&x>0){
+          x--;
+        }else
+        if(t==4&&x<m-1){
+          x++;
+        }else
+        if(x<m-1&&y<m-1){
+          x++;
+        }
       }
     }
-  }
+    }
 }
+
